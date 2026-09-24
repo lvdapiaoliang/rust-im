0568f20a1c15d4ab8d331e0d6d14792a589e78e1
lvguopeng <2977552486@qq.com>
2026-09-24 21:51:17 +0800

    项目初始化

---
+++ target/debug/examples/echo_demo-fc658d018a9ca462.d
@@ -0,0 +1,9 @@
+D:\0-develop\0-Rust\2-projects\rust-im\target\debug\examples\echo_demo-fc658d018a9ca462.d: crates\im-transport\examples\echo_demo.rs Cargo.toml
+
+D:\0-develop\0-Rust\2-projects\rust-im\target\debug\examples\libecho_demo-fc658d018a9ca462.rmeta: crates\im-transport\examples\echo_demo.rs Cargo.toml
+
+crates\im-transport\examples\echo_demo.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
