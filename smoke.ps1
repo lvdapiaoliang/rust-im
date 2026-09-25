92eea107e191f06aa276149901e3f84318a92b20
lvguopeng <2977552486@qq.com>
2026-09-25 14:37:55 +0800

    项目初始化

--- smoke.ps1
+++
@@ -1,8 +0,0 @@
-# JNI 冒烟（w11-e 复跑）：绕开命令直传时 -D 参数被终端解析层拆坏的坑
-$javaArgs = @(
-    '-Djava.library.path=target\release'
-    '-cp', 'target\java-classes'
-    'im.sdk.Demo'
-)
-& java @javaArgs
-exit $LASTEXITCODE
